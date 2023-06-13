35be0906a50e7d5acfdc44a1fb5cf0271b7593ae
Ultrasour8 <tristan.mattei@gmail.com>
2023-06-13 15:28:24 +0200

    Creation of the page "mentionsLégales" in the scr/pages + scss styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { DemoPageComponent } from './pages/demo/demo.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { DemoPipe } from './pipes/demo.pipe';
 import { DemoDirective } from './directives/demo.directive';
+import { MentionLegalesComponent } from './pages/mention-legales/mention-legales.component';
 
 @NgModule({
-  declarations: [AppComponent, DemoPageComponent, DemoComponent, DemoPipe, DemoDirective],
+  declarations: [AppComponent, DemoPageComponent, DemoComponent, DemoPipe, DemoDirective, MentionLegalesComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
