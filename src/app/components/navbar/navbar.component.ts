a74c140ea82f9c1249a0d6128163f5a856d6b17f
Emeric Bous <38536419+emericb@users.noreply.github.com>
2023-06-13 16:26:05 +0200

    updated styles

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  showOptions() {
+    const options = document.querySelector('.profile-btn');
+    if (options) options.classList.toggle('active');
+  }
 }
