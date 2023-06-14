ee436e14446c01cb43afb94548372422d13296cc
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-06-14 22:53:16 +0200

    form design done

--- src/app/components/homepage/homeform/homeform.component.ts
+++ src/app/components/homepage/homeform/homeform.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+// import { UilAt, UilLockAlt } from '@iconscout/unicons
 
 @Component({
   selector: 'app-homeform',
   templateUrl: './homeform.component.html',
   styleUrls: ['./homeform.component.scss'],
 })
-export class HomeformComponent {}
+export class HomeformComponent {
+  // UilAt = UilAt;
+  // UilLockAlt = UilLockAlt;
+}
