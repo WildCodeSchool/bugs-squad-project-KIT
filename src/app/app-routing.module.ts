02e3113f582997000b8f8937d7fc2e114279cb73
Ultrasour8 <tristan.mattei@gmail.com>
2023-06-15 12:28:47 +0200

    Adding Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DemoPageComponent } from './pages/demo/demo.component';
+import { MentionLegalesComponent } from './pages/mention-legales/mention-legales.component';
 
-const routes: Routes = [{ path: '', component: DemoPageComponent }];
+const routes: Routes = [
+  { path: '', component: DemoPageComponent },
+  { path: 'mention-legales', component: MentionLegalesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
