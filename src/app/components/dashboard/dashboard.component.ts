c03f58e2f3af13bb8907d9b1a9c2df9ac617a6d4
Emeric Bous <38536419+emericb@users.noreply.github.com>
2023-06-16 10:58:11 +0200

    add dashboard placeholder

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
