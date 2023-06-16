7204c019bd401ba7821ca5b124e07d2b06543592
Emeric Bous <38536419+emericb@users.noreply.github.com>
2023-06-16 13:14:56 +0200

    add route for dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RssComponent } from './pages/rss/rss.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [{ path: 'rss', component: RssComponent }];
-
+const routes: Routes = [
+  { path: 'rss', component: RssComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
