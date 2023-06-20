10465265de2235c057bd5a20fc922b4c88271fbe
Emeric Bous <38536419+emericb@users.noreply.github.com>
2023-06-20 11:46:41 +0200

    Update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { RssComponent } from './pages/rss/rss.component';
 import { Page500Component } from './pages/page500/page500.component';
 import { AboutComponent } from './pages/about/about.component';
 import { Page404Component } from './pages/page404/page404.component';
+import { MentionLegalesComponent } from './pages/mention-legales/mention-legales.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'rss', component: RssComponent },
   { path: 'collections', component: CollectionsComponent },
+  { path: 'mentionslegals', component: MentionLegalesComponent },
   { path: '404', component: Page404Component },
   { path: '500', component: Page500Component },
   { path: 'about', component: AboutComponent },
