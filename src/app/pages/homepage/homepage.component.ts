97f72273b43763c2b41be9d8e65d2d70ae274522
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-06-21 11:47:52 +0200

    change template to templateURL for homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -2,21 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
-  template: `
-    <header>
-      <div class="container">
-        <div class="upper">K.I.T.</div>
-        <div class="lower">K.I.T.</div>
-        <div class="inside">Keep it together</div>
-      </div>
-    </header>
-    <app-presentation></app-presentation>
-    <app-navbar *ngIf="false"></app-navbar>
-    <app-navbar-mobile *ngIf="false"></app-navbar-mobile>
-    <app-footer *ngIf="false"></app-footer>
-
-    <app-homeform></app-homeform>
-  `,
+  templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent {}
