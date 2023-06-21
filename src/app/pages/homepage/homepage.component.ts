3ce06ac65c7f5aa2cf2ecbc3eb648dec1beb06b4
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-06-21 09:02:58 +0200

    fix homepage postmerge bugs

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   template: `
-    <div class="container">
-      <div class="upper">K.I.T.</div>
-      <div class="lower">K.I.T.</div>
-      <div class="inside">Keep it together</div>
-    </div>
+    <header>
+      <div class="container">
+        <div class="upper">K.I.T.</div>
+        <div class="lower">K.I.T.</div>
+        <div class="inside">Keep it together</div>
+      </div>
+    </header>
     <app-presentation></app-presentation>
-    <!-- <app-scroll-button></app-scroll-button> -->
     <app-navbar *ngIf="false"></app-navbar>
     <app-navbar-mobile *ngIf="false"></app-navbar-mobile>
     <app-footer *ngIf="false"></app-footer>
