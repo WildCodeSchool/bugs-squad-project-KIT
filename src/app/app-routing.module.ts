66723ee0cfbaeb77dc04ab82174a9ad60c55819c
Emeric Bous <38536419+emericb@users.noreply.github.com>
2023-06-22 11:01:16 +0200

    fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,15 +10,48 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MentionLegalesComponent } from './pages/mention-legales/mention-legales.component';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent },
-  { path: 'rss', component: RssComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'collections', component: CollectionsComponent },
-  { path: 'legalNotices', component: MentionLegalesComponent },
-  { path: 'about', component: AboutComponent },
-  { path: '404', component: Page404Component },
-  { path: '500', component: Page500Component },
-  { path: '**', component: Page404Component },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomepageComponent,
+  },
+  {
+    path: 'rss',
+    component: RssComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'collections',
+    component: CollectionsComponent,
+  },
+  {
+    path: 'legalNotices',
+    component: MentionLegalesComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: '404',
+    component: Page404Component,
+  },
+  {
+    path: '500',
+    component: Page500Component,
+  },
+  {
+    path: '**',
+    component: Page404Component,
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
