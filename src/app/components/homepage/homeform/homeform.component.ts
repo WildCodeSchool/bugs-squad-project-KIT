38deedd72e33851c37974bc36d7b252ef05ba4dc
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-07-05 16:57:47 +0200

    reactive-form added, registe inputs changed

--- src/app/components/homepage/homeform/homeform.component.ts
+++ src/app/components/homepage/homeform/homeform.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-homeform',
   templateUrl: './homeform.component.html',
   styleUrls: ['./homeform.component.scss'],
 })
-export class HomeformComponent {}
+export class HomeformComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+
+  registerForm = new FormGroup({
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+
+  login() {
+    // CALL API with username and password
+    if (this.loginForm.invalid) return;
+    alert('Calling backend to login');
+  }
+
+  register() {
+    if (this.registerForm.invalid) return;
+    alert('Calling backend to register');
+  }
+}
