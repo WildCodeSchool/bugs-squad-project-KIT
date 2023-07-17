d3014b58f9d058242b5c31c9585095c167eb2f54
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-07-17 16:40:07 +0200

    default placeholder added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { GridStack } from 'gridstack';
 declare let $: any;
 
@@ -8,30 +8,18 @@ declare let $: any;
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  // @ViewChild('item1', { static: true }) item1!: ElementRef;
+  // @ViewChild('item2', { static: true }) item2!: ElementRef;
+  // @ViewChild('item3', { static: true }) item3!: ElementRef;
   ngOnInit(): void {
     setTimeout(() => {
       const grid = GridStack.init({
-        cellHeight: 70,
+        cellHeight: 100,
         acceptWidgets: true,
         removable: '#trash',
         float: true,
       });
       GridStack.setupDragIn('.newWidget', { appendTo: 'body', helper: 'clone' });
-
-      const items = [
-        { x: 0, y: 0, w: 4, h: 2, content: '1' },
-        { x: 4, y: 0, w: 4, h: 4, content: '2' },
-        { x: 8, y: 0, w: 2, h: 2, content: '3' },
-        { x: 10, y: 0, w: 2, h: 2, content: '4' },
-        { x: 0, y: 2, w: 2, h: 2, content: '5' },
-        { x: 2, y: 2, w: 2, h: 4, content: '6' },
-        { x: 8, y: 2, w: 4, h: 2, content: '7' },
-        { x: 0, y: 4, w: 2, h: 2, content: '8' },
-        { x: 4, y: 4, w: 4, h: 2, content: '9' },
-        { x: 8, y: 4, w: 2, h: 2, content: '10' },
-        { x: 10, y: 4, w: 2, h: 2, content: '11' },
-      ];
-      grid.load(items);
 
       grid.on('added removed change', function (e: any, items: any) {
         let str = '';
