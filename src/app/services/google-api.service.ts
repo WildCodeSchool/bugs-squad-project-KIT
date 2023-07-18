1a0ffef6b962a1f9f81c00faf67b04123c24a9d9
MatteiT <107050929+MatteiT@users.noreply.github.com>
2023-07-18 13:39:13 +0200

    Init Done with calendar TODO : JWT in header & guards

--- src/app/services/google-api.service.ts
+++ src/app/services/google-api.service.ts
@@ -28,21 +28,11 @@ export interface UserInfo {
 export class GoogleApiService {
   userProfileSubject = new Subject<UserInfo>();
 
-  constructor(private readonly oAuthService: OAuthService, private readonly httpClient: HttpClient) {
-    // confiure oauth2 service
+  constructor(private readonly oAuthService: OAuthService) {
     oAuthService.configure(authCodeFlowConfig);
-    // manually configure a logout url, because googles discovery document does not provide it
     oAuthService.logoutUrl = 'https://www.google.com/accounts/Logout';
-
-    // loading the discovery document from google, which contains all relevant URL for
-    // the OAuth flow, e.g. login url
     oAuthService.loadDiscoveryDocument().then(() => {
-      // // This method just tries to parse the token(s) within the url when
-      // // the auth-server redirects the user back to the web-app
-      // // It doesn't send the user the the login page
       oAuthService.tryLoginImplicitFlow().then(() => {
-        // when not logged in, redirecvt to google for login
-        // else load user profile
         oAuthService.loadUserProfile().then((userProfile) => {
           this.userProfileSubject.next(userProfile as UserInfo);
         });
