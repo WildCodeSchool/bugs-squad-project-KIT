416761893eaf92f3459dcf1ee5d6a9c15b2f040f
MatteiT <107050929+MatteiT@users.noreply.github.com>
2023-07-20 09:54:18 +0200

    Login and Register Tested

--- src/app/pipes/auth.interceptor.ts
+++ src/app/pipes/auth.interceptor.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { UserService } from '../services/user.service';
+import { Observable } from 'rxjs';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private oauthService: OAuthService) {}
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.userService.getToken();
-    if (token) {
-      const authRequest = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      return next.handle(authRequest);
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Get the auth token from the service.
+    let authToken = this.userService.getToken();
+    if (!authToken) {
+      authToken = this.oauthService.getAccessToken();
     }
-    return next.handle(request);
+
+    // Clone the request and replace the original headers with
+    // cloned headers, updated with the authorization.
+    const authReq = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${authToken}`),
+    });
+
+    // send cloned request with header to the next handler.
+    return next.handle(authReq);
   }
 }
