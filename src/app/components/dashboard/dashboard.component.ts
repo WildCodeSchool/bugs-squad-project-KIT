34691014d6b2fe7adc73781cb6a5ea71f599a271
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-07-24 12:20:42 +0200

    fix component display

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { GoogleApiService, UserInfo } from '../../services/google-api.service';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { HttpClient } from '@angular/common/http';
 import { GridStack } from 'gridstack';
 import { GridStackService } from 'src/app/services/grid-stack-service.service';
+import { animate, style, transition, trigger, state } from '@angular/animations';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state(
+        'in',
+        style({
+          overflow: 'hidden',
+          height: '*',
+          width: '300px',
+        })
+      ),
+      state(
+        'out',
+        style({
+          opacity: '0',
+          overflow: 'hidden',
+          height: '0px',
+          width: '0px',
+        })
+      ),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out')),
+    ]),
+  ],
 })
 export class DashboardComponent implements OnInit {
   userInfo?: UserInfo;
   private grid!: GridStack;
   private isInitialWidgetAdded = false;
+  private initialWidgets: Element[] = [];
+  helpMenuOpen!: string;
 
   constructor(
     private gridStackService: GridStackService,
@@ -37,6 +63,7 @@ export class DashboardComponent implements OnInit {
       });
     });
     this.initializeGrid();
+    this.helpMenuOpen = 'out';
   }
 
   private initializeGrid() {
@@ -47,21 +74,26 @@ export class DashboardComponent implements OnInit {
         float: true,
       });
 
-      GridStack.setupDragIn('.newWidget', { appendTo: 'body', helper: 'clone' });
+      GridStack.setupDragIn('.newWidget', {
+        appendTo: 'body',
+        // helper: 'clone'
+      });
 
       this.grid.on('added', (e: any, items: any) => {
-        let str = '';
         items.forEach((item: any) => {
-          str += ' (x,y)=' + item.x + ',' + item.y;
           const element = item.el;
           if (element.classList.contains('newWidget')) {
-            if (!this.isInitialWidgetAdded) {
+            // Vérifier si l'élément est déjà présent dans le tableau initialWidgets
+            if (!this.initialWidgets.includes(element)) {
+              this.initialWidgets.push(element);
               element.classList.add('dragged');
-              this.isInitialWidgetAdded = true;
             }
           }
         });
       });
+
+      const sidebarElement = this.sidebarElement.nativeElement as HTMLElement;
+      this.initialWidgets = Array.from(sidebarElement.querySelectorAll('.newWidget'));
     });
   }
 
@@ -97,5 +129,21 @@ export class DashboardComponent implements OnInit {
   isDivVisible = true;
   toggleDiv() {
     this.isDivVisible = !this.isDivVisible;
+  }
+
+  // Ajoutez un ViewChild pour obtenir une référence à l'élément avec la classe "sidebar"
+  @ViewChild('sidebar', { static: true }) sidebarElement!: ElementRef;
+
+  showContent = true; // Nouvelle variable pour contrôler la visibilité du contenu
+  // Méthode pour gérer l'événement de déplacement du widget de la classe "sidebar" à "grid-stack"
+  handleMoveToGridStack() {
+    // Vérifiez si la hauteur du parent dépasse 200px
+    const parentHeight = this.sidebarElement.nativeElement.offsetHeight;
+    if (parentHeight > 200) {
+      this.showContent = false;
+    }
+  }
+  toggleHelpMenu(): void {
+    this.helpMenuOpen = this.helpMenuOpen === 'out' ? 'in' : 'out';
   }
 }
