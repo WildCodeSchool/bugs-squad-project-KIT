ebd299dcac7fdca2d328a204e2c3949616dcb159
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-07-24 14:35:58 +0200

    add mail placeholder

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -41,6 +41,8 @@ export class DashboardComponent implements OnInit {
   private initialWidgets: Element[] = [];
   collecOpen!: string;
   todoOpen!: string;
+  rssOpen!: string;
+  mailOpen!: string;
 
   constructor(
     private gridStackService: GridStackService,
@@ -66,6 +68,8 @@ export class DashboardComponent implements OnInit {
     this.initializeGrid();
     this.collecOpen = 'out';
     this.todoOpen = 'out';
+    this.rssOpen = 'out';
+    this.mailOpen = 'out';
   }
 
   private initializeGrid() {
@@ -135,5 +139,13 @@ export class DashboardComponent implements OnInit {
   }
   toggletodo(): void {
     this.todoOpen = this.todoOpen === 'out' ? 'in' : 'out';
+  }
+
+  toggleMail(): void {
+    this.mailOpen = this.mailOpen === 'out' ? 'in' : 'out';
+  }
+
+  toggleRSS(): void {
+    this.rssOpen = this.rssOpen === 'out' ? 'in' : 'out';
   }
 }
